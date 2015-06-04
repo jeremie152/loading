562c1bda9deb4f09658e260b109ac4f82ff91c91
jeremie152 <lestueurfous@gmail.com>
2015-06-05 00:26:08 +0200

    loading

--- js/config.js
+++ js/config.js
@@ -83,11 +83,11 @@ in milliseconds*/
 var l_bgImageFadeVelocity = 2000;
 
 /*Enable background overlay?*/
-var l_bgOverlay = true;
+var l_bgOverlay = false;
 
 /*Background darkening amount
 0 for none (0%), 100 for pitch black (100%)*/
-var l_bgDarkening = 50;
+var l_bgDarkening = 0;
 
 /*Music player
 'true' or 'false' to enable music in the background*/
