3d9f03b7f3dc340bf037defb5aebc7e17e849518
jeremie152 <lestueurfous@gmail.com>
2015-06-05 05:47:23 +0200

    loading

--- js/config.js
+++ js/config.js
@@ -68,9 +68,6 @@ var l_bgImages = [
 	"backgrounds/images/touhou09.jpg",
         "backgrounds/images/touhou10.jpg",
         "backgrounds/images/touhou11.jpg",
-	"backgrounds/images/touhou12.jpg",
-	"backgrounds/images/touhou13.jpg",
-        "backgrounds/images/touhou14.jpg",
 ];
 
 /*Random background images order?*/
