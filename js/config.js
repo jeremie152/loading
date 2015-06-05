6f38acefd5beffc637bc0c79a524e33a587be839
jeremie152 <lestueurfous@gmail.com>
2015-06-05 03:45:00 +0200

    loading

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ var l_serverImage = "images/logo.jpg";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = 
+var l_serverName = "sleekLoad 2";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
