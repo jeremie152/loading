b06ba114d9a6b6f680020ab648a65f6ca8863055
jeremie152 <lestueurfous@gmail.com>
2015-06-05 04:47:30 +0200

    loading

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 ﻿/*
 --------------------------------------
---------LEAKED BY ANONYMOUS LEAKR --------------
+--------[FR] Apocalypse vs Ryoken DarkRP Serious [FR] --------------
 ----------------------------------------
       _           _    _                     _ 
      | |         | |  | |                   | |
@@ -12,7 +12,7 @@
 	
 
 --------------------------------------
---------LEAKED BY ANONYMOUS LEAKR --------------
+--------[FR] Apocalypse vs Ryoken DarkRP Serious [FR] --------------
 ----------------------------------------	
 */
 
@@ -22,7 +22,7 @@ var l_serverImage = "images/logo.jpg";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "sleekLoad 2";
+var l_serverName = "[FR] Apocalypse vs Ryoken DarkRP Serious [FR]";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
