c985d232aac33e4646ffe0c2273c0266cd3e7635
jeremie152 <lestueurfous@gmail.com>
2015-06-08 15:28:20 +0200

    loading

--- js/config.js
+++ js/config.js
@@ -109,7 +109,7 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 100;
+var l_musicVolume = 50;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
